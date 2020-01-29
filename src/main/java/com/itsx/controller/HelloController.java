00baea70bb8a1b93eda1c7be33b27d1e13846090
itsx <community@itsx.com>
2020-01-29 22:23:15 +0800

    application and dto and controller

--- src/main/java/com/itsx/controller/HelloController.java
+++ src/main/java/com/itsx/controller/HelloController.java
@@ -1,17 +1,14 @@
 package com.itsx.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public  String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "index";
+    public  String hello(){
+        return "hello";
     }
 
 }
